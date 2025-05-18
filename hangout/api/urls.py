39a9d5c64ba6b9bdec071527c6c77813157780c4
MNedkov09 <martin.nedkov2009@gmail.com>
2025-05-18 21:39:03 +0300

    added Views and Routing

--- hangout/api/urls.py
+++ hangout/api/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import EventViewSet, OptionViewSet, VoteViewSet, ParticipantViewSet
+
+router = DefaultRouter()
+router.register(r'events', EventViewSet)
+router.register(r'options', OptionViewSet)
+router.register(r'votes', VoteViewSet)
+router.register(r'participants', ParticipantViewSet)
 
 urlpatterns = [
-    path('', views.index), 
+    path('', include(router.urls)),
 ]
--- hangout/api/views.py
+++ hangout/api/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Event, Option, Vote, Participant
+from .serializers import EventSerializer, OptionSerializer, VoteSerializer, ParticipantSerializer
 
 # Create your views here.
 from django.http import JsonResponse
 
-def index(request):
-    return JsonResponse({"message": "Welcome to Hangout API"})
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all().order_by('-created_at')
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+class OptionViewSet(viewsets.ModelViewSet):
+    queryset = Option.objects.all()
+    serializer_class = OptionSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class VoteViewSet(viewsets.ModelViewSet):
+    queryset = Vote.objects.all()
+    serializer_class = VoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class ParticipantViewSet(viewsets.ModelViewSet):
+    queryset = Participant.objects.all()
+    serializer_class = ParticipantSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
