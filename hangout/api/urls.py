4fb976722b4a93098956d6a933655eba53ab25d7
MNedkov09 <martin.nedkov2009@gmail.com>
2025-05-26 16:27:53 +0300

    added register and login page

--- hangout/api/urls.py
+++ hangout/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import EventViewSet, OptionViewSet, VoteViewSet, ParticipantViewSet
+from .views import register_view, login_view
 
 router = DefaultRouter()
 router.register(r'events', EventViewSet)
@@ -10,4 +11,6 @@ router.register(r'participants', ParticipantViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path("register/", register_view, name="register"),
+    path("login/", login_view, name="login"),
 ]
--- hangout/api/views.py
+++ hangout/api/views.py
@@ -1,9 +1,37 @@
 from rest_framework import viewsets, permissions
 from .models import Event, Option, Vote, Participant
 from .serializers import EventSerializer, OptionSerializer, VoteSerializer, ParticipantSerializer
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 from django.http import JsonResponse
+
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists.")
+        else:
+            user = User.objects.create_user(username=username, password=password)
+            login(request, user)
+            return redirect("/")
+    return render(request, "register.html")
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/") 
+        else:
+            messages.error(request, "Invalid credentials.")
+    return render(request, "login.html")
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all().order_by('-created_at')
